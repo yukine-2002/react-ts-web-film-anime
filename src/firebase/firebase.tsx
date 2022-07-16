e13ac5dcc9f93ccf3a747c4dfbc3219bd32238fa
Bùi Thành Senpai <bthanh2701@gmail.com>
2022-07-16 23:47:46 +0700

    cap nhat 1 so chuc nang

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -41,4 +41,24 @@ const firebaseConfig = {
   providerFacebook.setCustomParameters({
     'display': 'popup'
   });
-  export const signInWithFacebook = () =>  signInWithPopup(auth, providerFacebook)
+  export const signInWithFacebook = () =>  signInWithPopup(auth, providerFacebook).then((result) => {
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user)
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const accessToken = credential!.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
+  });
